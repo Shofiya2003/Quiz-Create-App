42c3450a876ff6afb86299ada88bda10426c4898
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 10:56:31 +0530

    added the heading

--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -4,6 +4,7 @@ export default function QuizCard() {
   return (
     <main className='quiz-card full flex items-center'>
         <Date></Date>
+        <h1 className='question-heading'>Ouestion 1</h1>
     </main>
   )
 }
