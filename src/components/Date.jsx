ec90834117b3aac27947b9621cade397070e9006
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 13:54:38 +0530

    added the date functionality

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Date() {
+export default function Date(props) {
+  const {onChange,value}=props;
   return (
     <div>
-        <input className='date bg-light round-border text-white' type="date" name="date" id="date" />
+        <input value={value} onChange={onChange} className='date bg-light round-border text-white' type="date" name="date" id="date" />
     </div>
   )
 }
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Date from './Date'
 import Option from './Option'
 import OptionSelector from './OptionSelector'
 import NavButton from './NavButton'
 export default function QuizCard() {
+    const [date,setDate]=useState("");
+    useEffect(()=>{
+        console.log(date);
+    },[date])
   return (
     <main className='quiz-card full flex items-center'>
-        <Date></Date>
+        <Date value={date} onChange={(e)=>{
+            setDate(e.target.value);
+        }}></Date>
         <p className='question-heading'>Ouestion 1</p>
         <section className='question-section flex items-center'>
             <input className='bg-light round-border full-width text-white' placeholder='Question' type="text" name="question" id="question" />
