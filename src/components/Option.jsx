0ecb2b29ff2e18c2d507f2a113e0e4168252eb45
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 11:30:56 +0530

    added the options and styling

---
+++ src/components/Option.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Option() {
+  return (
+    <input type="text" className='option bg-light round-border' placeholder='First Option'/>
+  )
+}
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Date from './Date'
+import Option from './Option'
 export default function QuizCard() {
   return (
     <main className='quiz-card full flex items-center'>
@@ -7,6 +8,17 @@ export default function QuizCard() {
         <h1 className='question-heading'>Ouestion 1</h1>
         <section className='question-section flex items-center'>
             <input className='bg-light round-border full-width text-white' placeholder='Question' type="text" name="question" id="question" />
+            <div className='full-width'>
+                <div className='options-subsection flex'>
+                    <Option></Option>
+                    <Option></Option>
+                </div>
+                <div className='options-subsection flex'>
+                    <Option></Option>
+                    <Option></Option>
+                </div>
+            </div>
+
         </section>
     </main>
   )
