83069df2b1b0c4149e3fad0883ad61b10ee3a207
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 13:50:33 +0530

    added the control buttons

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavButton() {
+  return (
+    <div className='bg-light round-border flex justify-center nav-button'>
+        Add Question
+    </div>
+  )
+}
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Date from './Date'
 import Option from './Option'
 import OptionSelector from './OptionSelector'
+import NavButton from './NavButton'
 export default function QuizCard() {
   return (
     <main className='quiz-card full flex items-center'>
         <Date></Date>
-        <h1 className='question-heading'>Ouestion 1</h1>
+        <p className='question-heading'>Ouestion 1</p>
         <section className='question-section flex items-center'>
             <input className='bg-light round-border full-width text-white' placeholder='Question' type="text" name="question" id="question" />
             <div className='full-width'>
@@ -22,6 +23,14 @@ export default function QuizCard() {
             <OptionSelector></OptionSelector>
             <textarea className='bg-light round-border full-width' placeholder='Hint'></textarea>
             <input className='bg-light round-border full-width file' type="file" />
+        </section>
+        <section className='controls'>
+            <div className='round-border full-width delete-button action-button flex justify-center'>Delete</div>
+             <div className='flex full-width' style={{"gap":"52px"}}>
+                <NavButton></NavButton>
+                <NavButton></NavButton>
+             </div>
+            <div className='round-border full-width submit-button action-button flex justify-center'>Submit</div>
         </section>
     </main>
   )
