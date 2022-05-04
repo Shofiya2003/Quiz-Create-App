fb3d4bd7fddd5e50a8c734c4697c73b47160735f
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 14:06:52 +0530

    made the add and removed functionality

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function NavButton() {
+export default function NavButton(props) {
+  const {onClick,text}=props;
   return (
-    <div className='bg-light round-border flex justify-center nav-button'>
-        Add Question
+    <div className='bg-light round-border flex justify-center nav-button' onClick={onClick}>
+        {text}
     </div>
   )
 }
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -5,15 +5,33 @@ import OptionSelector from './OptionSelector'
 import NavButton from './NavButton'
 export default function QuizCard() {
     const [date,setDate]=useState("");
+    const [quizArr,setQuizArr]=useState([]);
+
+    //add a question
+    const onAdd=()=>{
+        setQuizArr(prev=>{
+            const updatedQuizArr=[...prev,{}];
+            return updatedQuizArr;
+        })
+    }
+
+    //remove last question
+    const onRemove=()=>{
+        setQuizArr((prev)=>{
+            prev.pop();
+            return [...prev];
+        })
+    }
+    
     useEffect(()=>{
-        console.log(date);
-    },[date])
+        console.log(quizArr);
+    },[quizArr])
   return (
     <main className='quiz-card full flex items-center'>
         <Date value={date} onChange={(e)=>{
             setDate(e.target.value);
         }}></Date>
-        <p className='question-heading'>Ouestion 1</p>
+        <p className='question-heading'>Ouestion {quizArr.length+1}</p>
         <section className='question-section flex items-center'>
             <input className='bg-light round-border full-width text-white' placeholder='Question' type="text" name="question" id="question" />
             <div className='full-width'>
@@ -33,8 +51,8 @@ export default function QuizCard() {
         <section className='controls'>
             <div className='round-border full-width delete-button action-button flex justify-center'>Delete</div>
              <div className='flex full-width' style={{"gap":"52px"}}>
-                <NavButton></NavButton>
-                <NavButton></NavButton>
+                <NavButton onClick={onAdd} text={"Add Question"}></NavButton>
+                <NavButton onClick={onRemove} text={"Remove Last Question"}></NavButton>
              </div>
             <div className='round-border full-width submit-button action-button flex justify-center'>Submit</div>
         </section>
