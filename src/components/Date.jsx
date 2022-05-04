8f67dc875b6f7385eaec2f926b5c1f7b3b9bef91
Shofiya2003 <shofiyabootwala@gmail.com>
2022-05-04 11:07:46 +0530

    added the question input box with styling

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Date() {
   return (
     <div>
-        <input className='date round-border text-white' type="date" name="date" id="date" />
+        <input className='date bg-light round-border text-white' type="date" name="date" id="date" />
     </div>
   )
 }
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -5,6 +5,9 @@ export default function QuizCard() {
     <main className='quiz-card full flex items-center'>
         <Date></Date>
         <h1 className='question-heading'>Ouestion 1</h1>
+        <section className='question-section flex items-center'>
+            <input className='bg-light round-border full-width text-white' placeholder='Question' type="text" name="question" id="question" />
+        </section>
     </main>
   )
 }
